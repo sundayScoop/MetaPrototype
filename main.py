c78a91e98336bfcc96e74203733cdfed5a03eb7f
Julio <julio@tide.org>
2022-10-26 19:26:15 +1000

    change

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from functions import *
-
 from ed25519 import *
 
-a = 'hey'
-b = 'ho'
-
-key = 100
-
-sig = hmac_sign(a, b, key=key)
-
-verif = hmac_verify(a, b, hashed=sig, key=key)
-print(verif)
+'''
+Prototype here
+'''
